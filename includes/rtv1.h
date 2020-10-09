77f4f1d6902becab047d33777c0e1c0952e5f3d9
mashley <andrey.korchmarek@yandex.ru>
2020-10-09 18:33:43 +0300

    small changes

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -2,7 +2,25 @@
 // Created by Mari Ashley on 8/18/20.
 //
 
-#ifndef RTV1_RTV1_H
-#define RTV1_RTV1_H
+#ifndef RTV1_H
+#define RTV1_H
+#include <stdio.h>
+#include <math.h>
+#define WIN_H (300)
+#define WIN_W (400)
+#define WIN_SIZE (WIN_H * WIN_W)
 
-#endif //RTV1_RTV1_H
+typedef struct		s_coord
+{
+	double	x;
+	double	y;
+	double	z;
+}			t_coord;
+
+typedef	struct	s_sphere
+{
+	t_coord	center;
+	double	radius;
+}				t_sphere;
+
+#endif
--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include "rtv1.h"
 
+double	square_vector_lenght(t_coord start, t_coord end)
+{
+	return (pow((end.x - start.x), 2) + pow((end.y - start.y), 2) +
+	pow((end.z - start.z), 2));
+}
+
 int	main(void)
 {
 	return (0);
