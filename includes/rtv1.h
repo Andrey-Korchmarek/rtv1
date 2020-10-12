e075498d1bd631b4082bdfdf0382dce52c0c7a99
Джон Мендрейк <mendreik@KAS.MO-Kor-Gk-330-AR>
2020-10-12 10:03:32 +0300

    sunday

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -5,6 +5,7 @@
 #ifndef RTV1_H
 #define RTV1_H
 #include <stdio.h>
+# include <stdlib.h>
 #include <math.h>
 #define WIN_H (300)
 #define WIN_W (400)
@@ -17,10 +18,22 @@ typedef struct		s_coord
 	double	z;
 }			t_coord;
 
+typedef	struct	s_vector
+{
+	t_coord	beg;
+	t_coord	end;
+}				t_vector;
+
 typedef	struct	s_sphere
 {
 	t_coord	center;
 	double	radius;
 }				t_sphere;
+
+typedef	struct	s_rtv
+{
+	t_coord	o;
+	double	d;
+}				t_rtv;
 
 #endif
--- main.c
+++ main.c
@@ -1,12 +1,71 @@
-#include "rtv1.h"
+#include "includes/rtv1.h"
 
-double	square_vector_lenght(t_coord start, t_coord end)
+t_coord	win_to_point(int i, int j, int d)
 {
-	return (pow((end.x - start.x), 2) + pow((end.y - start.y), 2) +
-	pow((end.z - start.z), 2));
+	double x;
+	double y;
+	double z;
+
+	x = WIN_W / 2 + i;
+	y = WIN_H / 2 - j;
+	z = d;
+	return ((t_coord){x, y, z});
+}
+
+double	scalar_product(t_vector a, t_vector b)
+{
+	double x;
+	double y;
+	double z;
+
+	x = fabs(a.end.x - a.beg.x) * fabs(b.end.x - b.beg.x);
+	y = fabs(a.end.y - a.beg.y) * fabs(b.end.y - b.beg.y);
+	z = fabs(a.end.z - a.beg.z) * fabs(b.end.z - b.beg.z);
+	return (x + y + z);
+}
+
+void	change_pixel(t_rtv *data, int *pix)
+{
+//	double
+	return ;
+}
+
+int	**get_window(t_rtv *data)
+{
+	int **scene;
+	int i;
+	int j;
+
+	scene = (int**) malloc(sizeof(int) * WIN_SIZE);
+	i = 0;
+	while (i < WIN_W)
+	{
+		scene[i] = (int*)malloc(sizeof(int) * WIN_H);
+		j = 0;
+		while (j < WIN_H)
+		{
+			scene[i][j] = 0xFFFFFF;
+			j++;
+		}
+		i++;
+	}
+	return (scene);
+}
+
+void	set_default(t_rtv *data)
+{
+//	data->o = (t_)
+	data = NULL;
+	return ;
 }
 
 int	main(void)
 {
+	t_rtv	*data;
+	int		**scene;
+
+	data = (t_rtv*)malloc(sizeof(t_rtv));
+	set_default(data);
+	scene = get_window(data);
 	return (0);
 }
