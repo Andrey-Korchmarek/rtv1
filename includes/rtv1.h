fde810e90ea47541eb1cdd1b3ea7e96a0d9ae182
mashley <andrey.korchmarek@yandex.ru>
2020-11-26 09:17:31 +0300

    g

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "libft.h"
+#include "../libft/includes/libft.h"
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 #define WIN_H (30)
